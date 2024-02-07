961b64c365c29fc57c4b550f970eb6fa4ab87d1f
Victor Queiroga <victorqueiroga.jp@gmail.com>
2024-02-06 22:48:02 -0300

    adição de categorias

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,10 @@
 import {useState} from "react"
 import { CategoryButton } from "@/components/category-button"
+
 import { Header } from "@/components/header"
-import { View, FlatList } from "react-native"
-import { CATEGORIES } from "@/utils/data/products"
+import { View, FlatList, SectionList, Text } from "react-native"
+import { CATEGORIES, MENU } from "@/utils/data/products"
+import { Product } from "@/components/product"
 
 
 export default function Home() {
@@ -15,20 +17,22 @@ function hangleCategorySelect(selectedCategory: string) {
 }
 
     return (
-        <View className="flex-1 pt-8" >
+        <View className="pt-8" >
             <Header title="Faça seu pedido" cartQuantityItems={3} />
-
-
-            {/*
-            <View className="flex-row gap-4">
-                <CategoryButton title="Lanche do dia" />
-            </View>
-    */}
-
-
             <FlatList data={CATEGORIES} keyExtractor={(item) => item} renderItem={({ item }) => (
                 <CategoryButton title={item} isSelected={item === category} onPress={() => hangleCategorySelect(item)}/>
-            )} horizontal className="max-w-10 mt-5" contentContainerStyle={{gap: 12, paddingHorizontal: 20}} showsHorizontalScrollIndicator={false}/>
+            )} horizontal className="max-w-10 mt-5 h-10"  contentContainerStyle={{gap: 12, paddingHorizontal: 20}} showsHorizontalScrollIndicator={false}/>
+
+            <SectionList sections={MENU} 
+                keyExtractor={(item) => item.id} 
+                stickySectionHeadersEnabled={false} 
+                renderItem={({item}) => (
+                  <Product data={item} ></Product>
+                )}
+                renderSectionHeader={({section : {title}}) => <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>} 
+                className="p-5" showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom: 200}}>
+                    
+            </SectionList>
 
         </View>
     )
